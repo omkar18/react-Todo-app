e58e3b705cdfefc6d29a40f40a00b4ee38106783
omkar walujkar <omkarwalujkar18@gmail.com>
2019-09-14 14:38:04 +0530

    create build folder

---
+++ build/precache-manifest.7237f3918dacca65d4cf66a4aec5528b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cff64ef8770a1222c28d5d9c19f876c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6123d0b0b3b59dad6de",
+    "url": "/static/css/main.fc29d31f.chunk.css"
+  },
+  {
+    "revision": "c943ac700fb7d44ed853",
+    "url": "/static/js/2.309076b9.chunk.js"
+  },
+  {
+    "revision": "c6123d0b0b3b59dad6de",
+    "url": "/static/js/main.74ade266.chunk.js"
+  },
+  {
+    "revision": "a82607262ddef3b7683b",
+    "url": "/static/js/runtime~main.de838ab2.js"
+  }
+]);
