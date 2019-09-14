2085ceb7b58d0a4a73967479c26ce44068e9b888
omkar walujkar <omkarwalujkar18@gmail.com>
2019-09-14 14:40:04 +0530

    Updates

---
+++ precache-manifest.1f1e6c51841c2030731b3ffd39a031e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aadfb685afc7965b472a070a4383eb6a",
+    "url": "/react-Todo-app/index.html"
+  },
+  {
+    "revision": "70952574c2a2835292e9",
+    "url": "/react-Todo-app/static/css/main.fc29d31f.chunk.css"
+  },
+  {
+    "revision": "c943ac700fb7d44ed853",
+    "url": "/react-Todo-app/static/js/2.309076b9.chunk.js"
+  },
+  {
+    "revision": "70952574c2a2835292e9",
+    "url": "/react-Todo-app/static/js/main.9099d506.chunk.js"
+  },
+  {
+    "revision": "f44ded30928f138f81dc",
+    "url": "/react-Todo-app/static/js/runtime~main.88ccb60c.js"
+  }
+]);
